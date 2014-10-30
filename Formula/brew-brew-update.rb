71a37c27e1bc4028e114d4770201a400a976a86f
The Continium <thecontinium@outlook.com>
2014-10-30 11:50:22 +1100

    Initial Upload

---
+++ Formula/brew-brew-update.rb
@@ -0,0 +1,51 @@
+outlook
+
+class BrewUpdate < Formula
+  homepage 'https://github.com/thecontinium/homebrew-update/'
+  url 'https://github.com/thecontinium/homebrew-update.git'
+  version '0.0.1'
+
+  skip_clean 'bin'
+
+  def install
+    bin.install 'homebrewupdate'
+  end
+
+  plist_options :startup => true
+
+  def plist; <<-EOS.undent
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Label</key>
+      <string>#{plist_name}</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>/usr/local/bin/homebrewupdate</string>
+	</array>
+	<key>StandardErrorPath</key>
+	<string>/dev/null</string>
+	<key>StandardOutPath</key>
+	<string>/dev/null</string>
+	<key>StartCalendarInterval</key>
+	<array>
+		<dict>
+			<key>Hour</key>
+			<integer>21</integer>
+			<key>Minute</key>
+			<integer>0</integer>
+		</dict>
+		<dict>
+			<key>Hour</key>
+			<integer>9</integer>
+			<key>Minute</key>
+			<integer>0</integer>
+		</dict>
+	</array>
+</dict>
+</plist>
+    EOS
+  end
+
+end
