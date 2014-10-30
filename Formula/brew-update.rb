747cab1e1919dd76b2abf68c8645d3c09d0c651f
The Continium <thecontinium@outlook.com>
2014-10-30 13:00:05 +1100

    updated plist

--- Formula/brew-update.rb
+++ Formula/brew-update.rb
@@ -12,37 +12,37 @@ class BrewUpdate < Formula
   plist_options :startup => true
 
   def plist; <<-EOS.undent
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>Label</key>
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+    	<key>Label</key>
       <string>#{plist_name}</string>
-	<key>ProgramArguments</key>
-	<array>
-		<string>/usr/local/bin/homebrewupdate</string>
-	</array>
-	<key>StandardErrorPath</key>
-	<string>/dev/null</string>
-	<key>StandardOutPath</key>
-	<string>/dev/null</string>
-	<key>StartCalendarInterval</key>
-	<array>
-		<dict>
-			<key>Hour</key>
-			<integer>21</integer>
-			<key>Minute</key>
-			<integer>0</integer>
-		</dict>
-		<dict>
-			<key>Hour</key>
-			<integer>9</integer>
-			<key>Minute</key>
-			<integer>0</integer>
-		</dict>
-	</array>
-</dict>
-</plist>
+    	<key>ProgramArguments</key>
+	    <array>
+		    <string>/usr/local/bin/homebrewupdate</string>
+	    </array>
+	    <key>StandardErrorPath</key>
+	    <string>/dev/null</string>
+	    <key>StandardOutPath</key>
+	    <string>/dev/null</string>
+	    <key>StartCalendarInterval</key>
+	    <array>
+		    <dict>
+			    <key>Hour</key>
+			    <integer>21</integer>
+			    <key>Minute</key>
+			    <integer>0</integer>
+		    </dict>
+		    <dict>
+		    	<key>Hour</key>
+		    	<integer>9</integer>
+			    <key>Minute</key>
+			    <integer>0</integer>
+		    </dict>
+	    </array>
+    </dict>
+    </plist>
     EOS
   end
 
