040b1d7368f2b29a4204fa396877c30c918b5712
thecontinium <thecontinium@outlook.com>
2015-12-22 11:14:25 +0530

    Increment version

--- Formula/brew-update.rb
+++ Formula/brew-update.rb
@@ -1,7 +1,7 @@
 class BrewUpdate < Formula
   homepage 'https://github.com/thecontinium/homebrew-brewupdate/'
   url 'https://github.com/thecontinium/homebrew-brewupdate.git'
-  version '0.0.4'
+  version '0.0.5'
 
   skip_clean 'bin'
 
