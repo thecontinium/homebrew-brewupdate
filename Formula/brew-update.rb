775ba45ad7e09e04afc440df607389363c4e0025
thecontinium <thecontinium@outlook.com>
2016-10-05 14:56:14 +0800

    join brew update output

--- Formula/brew-update.rb
+++ Formula/brew-update.rb
@@ -1,7 +1,7 @@
 class BrewUpdate < Formula
   homepage 'https://github.com/thecontinium/homebrew-brewupdate/'
   url 'https://github.com/thecontinium/homebrew-brewupdate.git'
-  version '0.1.0'
+  version '0.1.1'
 
   skip_clean 'bin'
 
