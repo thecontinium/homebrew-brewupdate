7f44ee22de3df5298602eb5c1a02281a5fe5892b
thecontinium <thecontinium@outlook.com>
2017-05-15 15:05:12 +0800

    Made output from json raw

--- Formula/brew-update.rb
+++ Formula/brew-update.rb
@@ -1,7 +1,7 @@
 class BrewUpdate < Formula
   homepage 'https://github.com/thecontinium/homebrew-brewupdate/'
   url 'https://github.com/thecontinium/homebrew-brewupdate.git'
-  version '0.1.12'
+  version '0.1.13'
 
   skip_clean 'bin'
 
