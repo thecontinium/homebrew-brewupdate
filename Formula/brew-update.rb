0a99bc0e3a97a2344f863e6ee665fd240a100f38
thecontinium <thecontinium@outlook.com>
2017-05-15 13:41:28 +0800

    Use json for the reply

--- Formula/brew-update.rb
+++ Formula/brew-update.rb
@@ -1,11 +1,12 @@
 class BrewUpdate < Formula
   homepage 'https://github.com/thecontinium/homebrew-brewupdate/'
   url 'https://github.com/thecontinium/homebrew-brewupdate.git'
-  version '0.1.10'
+  version '0.1.11'
 
   skip_clean 'bin'
 
   depends_on "terminal-notifier"
+  depends_on "jq"
 
 
   def install
